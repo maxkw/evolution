70a37238643d161c1bfbb0dd84f8e32dfafffe4f
Alejandro Vientos <polyguo@gmail.com>
2017-02-21 20:28:06 -0500

    fixed 'first impressions' experiment+plot

--- experiments.py
+++ experiments.py
@@ -6,12 +6,14 @@ import numpy as np
 
 from params import default_params,generate_proportional_genomes,default_genome
 from indirect_reciprocity import World,ReciprocalAgent,SelfishAgent,AltruisticAgent
-from games import RepeatedPrisonersTournament
+from games import RepeatedPrisonersTournament,BinaryDictator,Repeated,PrivatelyObserved,Symmetric
 from collections import defaultdict
 from itertools import combinations_with_replacement as combinations
+from itertools import permutations
 from itertools import product
 import matplotlib.pyplot as plt
 from numpy import array
+from copy import copy,deepcopy
 
 
 ###multi_call
@@ -209,8 +211,6 @@ def RAvRA_plot(data, save_dir="./plots/", save_file="RAvRA.pdf"):
 
 from games import RepeatedDynamicPrisoners
 def compare_RA(prior_lst = np.linspace(.25,.75,7)):
-    
-    
     size = [len(prior_lst)]*2
     prior_2_index = dict(map(reversed,enumerate(prior_lst)))
     lookup = lambda p: prior_2_index[p]
@@ -229,30 +229,174 @@ def compare_RA(prior_lst = np.linspace(.25,.75,7)):
     return arr
 
 from games import RepeatedSequentialBinary
+from indirect_reciprocity import NiceReciprocalAgent
 def comparison_plotter(prior_list = [.25,.50,.75]):
     priors = unordered_prior_combinations(prior_list)
     print priors
-    game = RepeatedDynamicPrisoners()#RepeatedSequentialBinary()
-    data = RAvRA(priors = priors, trial = 500, RA_K = 1, agent_types = [(SelfishAgent,ReciprocalAgent)],games = game)
+    game = RepeatedSequentialBinary()
+    data = RAvRA(priors = priors, trial = 100, RA_K = 2,agent_types = [(SelfishAgent,ReciprocalAgent,AltruisticAgent)],games = game)
     record = []
 
     rewards = sorted(list(set(sum(map(list,data['return']),[]))))
     for priors,group in data.groupby('priors'):
         p0,p1 = priors
         for r0,r1 in group['return']:
-            record.append({'recipient prior':p0,'opponent prior':p1,'reward':r0})
-            record.append({'recipient prior':p1,'opponent prior':p0,'reward':r1})
+            record.append({'recipient prior':p0, 'opponent prior':p1, 'reward':r0})
+            record.append({'recipient prior':p1, 'opponent prior':p0, 'reward':r1})
     data = pd.DataFrame(record)
+    
+    #data = data.pivot("recipient prior","opponent prior","reward")
+    #print data
+    meaned = data.groupby(['recipient prior','opponent prior']).mean().unstack()
+
 
     def meandrawer(data,**kwargs):
         plt.axvline(data.mean())
-    g = sns.FacetGrid(data,row='recipient prior',col='opponent prior',margin_titles=True)
-    g = (g.map(sns.kdeplot,"reward")
-        .map(meandrawer,"reward"))
+    #g = sns.FacetGrid(data,row='recipient prior',col='opponent prior',margin_titles=True)
+    #g = (g.map(sns.kdeplot,"reward")
+    #    .map(meandrawer,"reward"))
+    sns.heatmap(meaned,annot=True,fmt="f")
+    #plt.show()
     plt.savefig("./plots/compare.pdf")
+
+
+@multi_call(unordered = ['agent_types'])
+def first_impressions(RA_K = 2, RA_prior = .75, preactions = 5, kind = 'seq', agent_type = ReciprocalAgent, agent_types = [(ReciprocalAgent, SelfishAgent)],passive = False,trial = 50):
+    condition = locals()
+    params = default_params(game = RepeatedPrisonersTournament(10),**condition)
+    genomes = [default_genome(**condition)]*2
+
+    BD = BinaryDictator()
+    def int_to_actions_string(number):
+            """
+            given a number return a binary representation
+            where 0s are Ds and 1s are Cs
+            """
+            return ("{0:0"+str(preactions)+"b}").format(number).replace('1','C').replace('0','D')
+
+
+    if kind == 'seq':
+        action_strings = ["C"*preactions+"D"]
+        #action_strings = ["D"*3]
+    elif kind == 'perm':
+        action_strings = [int_to_actions_string(n) for n in range(preactions)]
+
+    action_strings = ["DDD"]
+
+    def char_to_observation(action_char):
+        action = "give" if action_char is "C" else "keep"
+        return [(BD,[0,1],[0,1],action)]
+
+    def observations(actions_string):
+        return map(char_to_observation,actions_string)
+
+
+
+
+    record = []
+    #have agents observe the prehistoric observations
+    #if second agent gets to react, have them act appropriately
+    for action_string in action_strings:
+    #make the prehistory and save the initial state of beliefs of agents
+
+        world = World(params, genomes)
+        agents = world.agents
+        action_len = len(action_string)
+        prehistory  = [{
+            'round':-action_len,
+            'belief': [copy(a.belief) for a in world.agents],
+            'prior': RA_prior,
+            'players': [deepcopy(agent) for agent in world.agents],
+        }]
+
+        for r,observation in enumerate(observations(action_string),-(action_len-1)):
+            if not passive:
+                new_observation = BD.play(world.agents[array([1,0])],world.agents,tremble = 0)[1]
+                observation = observation+new_observation
+            for agent in world.agents:
+                agent.observe(observation)
+
+            prehistory.append({
+                'round':r,
+                'belief': [copy(a.belief) for a in world.agents],
+                'prior': RA_prior,
+                'players': [deepcopy(agent) for agent in world.agents],
+            })
+
+        fitness,history = world.run()
+        record.append((action_string,prehistory+history))
+    
+    return record
+
+def fi_data_slice(data):
+
+    def belief_getter(agent,a_id,k):
+        o_id = (a_id+1)%2
+        #print k,n_id
+        if k == 0:
+            return agent.belief_that(o_id,ReciprocalAgent)
+        else:
+            return belief_getter(agent.model[o_id].agent,o_id,k-1)
+
+    ret = []
+    for record in data.to_dict('records'):
+        K = record['RA_K']
+        for action, history in record['return']:
+            for event in history:
+                for agent_id in [0,1]:
+                    agent = event['players'][agent_id]
+                    for k in range(int(K)+1):
+                        ret.append({
+                            'K': K,
+                            'k':k,
+                            'round': event['round'],
+                            'actions': action,
+                            'belief': belief_getter(agent,agent_id,k),
+                            'believer':agent_id,
+                            'type': "RA",
+                        })
+
+    return pd.DataFrame(ret)
+
+
+
+def first_impressions_plotter(out_dir = "./plots/"):
+    K = 1
+    data = fi_data_slice(first_impressions(trial = 100,kind = 'seq', RA_prior = .75, preactions = 3, RA_K=K,passive=False))
+    figure = sns.factorplot('round','belief', hue = 'type',#'actions',
+                            col='believer',row='k',data=data, ci=68,legend = False,aspect = 1, size = 4.5, #kind = 'point')
+                            kind = 'violin',scale ='area', width=.9,cut = 0,inner = 'point',bw = .2)
+    #figure = sns.factorplot('round','belief', hue = None, col='type',row='actions',data=data, ci=68,legend_out = True,aspect = 1.5,size = 5)#kind = 'violin',scale ='count', width=.9,cut = .5,inner = 'box')
+    #size = figure.get_size_inches()
+    #figure.set_size_inches((size[0]*1,size[1]*1.5))
+    figure.set(yticks=[x/10.0 for x in range(11)])
+    y_buff = .0
+    plt.ylim([0-y_buff,1+y_buff])
+    
+    figure.set_titles('','','')
+    id_to_letter = dict(enumerate("AB"))
+    """
+    range(3) bc k in [0,1,2]
+    a_id is the agent, t_id is who the agent is thinking about
+        """
+    for k,(a_id,t_id) in product(range(K+1),permutations(range(2))):
+        axis = figure.facet_axis(k,a_id)
+        sns.pointplot(x = "round", y="belief", color = "red",data = data[(data['believer']==a_id) & (data['k']==k)],ax=axis)
+        axis.set(#xlabel='# of interactions',
+            ylabel = '$\mathrm{Pr_{%s}( T_{%s} = RA | O_{1:n} )}$'% (k,id_to_letter[t_id]),
+            title = "%s's K=%s belief that %s is RA" % (id_to_letter[a_id],k,id_to_letter[t_id]))
+        
+    #figure.fig.subplots_adjust(top =.80)
+    plt.subplots_adjust(top = 0.93)
+    figure.fig.suptitle("A and B's beliefs that the other is RA when A's first 3 moves are D")
+    plt.show()
+    
+first_impressions_plotter()
+    
+    
 
 #compare_RA()
 #RAvRA_plot(RAvRA(trial = 1000))
-comparison_plotter()
+#comparison_plotter(np.linspace(0,1,5))
 #RAvRA_plot(RAvRA())
 #print RAvRA(trial=1)
