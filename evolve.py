c9fe4bbd1d3ca3d496fbb839845f0e7bc88a4c6d
Alejandro Vientos <polyguo@gmail.com>
2017-05-15 01:18:39 -0400

    ?

--- evolve.py
+++ evolve.py
@@ -10,7 +10,7 @@ from experiments import NiceReciprocalAgent, SelfishAgent, ReciprocalAgent, Altr
 from experiment_utils import multi_call, experiment, plotter, MultiArg, cplotter, memoize, apply_to_args
 import matplotlib.pyplot as plt
 import seaborn as sns
-from experiments import binary_matchup, memoize, matchup_matrix, matchup_plot
+from experiments import binary_matchup, memoize, matchup_matrix, matchup_plot,matchup_matrix_per_round
 from params import default_genome
 from indirect_reciprocity import gTFT, AllC, AllD, Pavlov, RandomAgent
 from params import default_params
@@ -123,19 +123,20 @@ def limit_v_sim_param(param, player_types, **kwargs):
                 "proportion":p
             })
     return record
-
-def limit_v_rounds(player_types, **kwargs):
-    max_rounds = kwargs.get('max_rounds',100)
-    matrices = matchup_matrix_per_round(player_types, max_rounds, **kwargs)
+@experiment(unpack = 'record', memoize = False)
+def limit_v_rounds(player_types, max_rounds = 100, trials = 50, **kwargs):
+    matrices = matchup_matrix_per_round(player_types, max_rounds = max_rounds, trials = trials, **kwargs)
     params = default_params(**kwargs)
     record = []
     for r, payoff in matrices:
+
         for t,p in zip(player_types, limit_analysis(payoff, **params)):
             record.append({
                 'rounds':r,
                 "type":t.short_name("agent_types"),
                 "proportion":p
             })
+    return record
 
 def limit_v_param(param,player_types,**kwargs):
     if param in ['beta','RA_prior']:
@@ -152,7 +153,6 @@ def limit_v_param(param,player_types,**kwargs):
 @plotter(limit_v_param, plot_exclusive_args = ['experiment','data'])
 def limit_param_plot(param, player_types, data = [], **kwargs):
     fig = plt.figure()
-    print data
     for hue in data['type'].unique():
         d = data[data['type']==hue]
         p = plt.plot(d[param], d['proportion'], label=hue)
@@ -179,7 +179,6 @@ def limit_v_bc(player_types,**kwargs):
     Bs = [1.2, 1.4, 1.6, 1.8, 2, 2.2, 2.4, 2.6, 2.8, 3]
     for b in Bs:
         payoff = matchup_matrix(player_types = player_types, benefit = b, **kwargs)
-        print payoff
         ssd = limit_analysis(payoff, **params)
         for t,p in zip(player_types,ssd):
             record.append({
@@ -190,10 +189,27 @@ def limit_v_bc(player_types,**kwargs):
     return record
 
 
-def cb_v_rounds(player_types, **kwargs):
-    max_rounds = 10
-    for b in [1.2, 1.4, 1.6, 1.8, 2, 2.2, 2.4, 2.6, 2.8, 3]:
-        pass
+def bc_v_rounds(player_types, max_rounds = 50, **kwargs):
+    params = default_params(**kwargs)
+    record = []
+    for b in [1.2, 1.4, 1.6, 1.8, 2, 2.2, 2.4, 2.6, 2.8, 3, 3.2, 3.4, 3.6]:
+        matrices = matchup_matrix_per_round(player_types, max_rounds, benefit = b, **kwargs)
+        for rounds, payoffs in matrices:
+            ssd = limit_analysis(payoffs, **params)
+            #winner = max(zip(ssd,player_types),key = lambda t: t[0])[1]
+            if rounds%2 == 0:
+                record.append({
+                    "benefit":b,
+                    "frequency":ssd[0],
+                    "rounds":rounds,
+                })
+    return pd.DataFrame(record).pivot(columns = 'benefit', index = 'rounds', values = 'frequency')
+
+
+@plotter(bc_v_rounds)
+def bc_rounds_plot(player_types,data=[],**kwargs):
+    plt.figure(figsize = (10,50))
+    sns.heatmap(data,annot=True,fmt="0.2f")
 
 def AllC_AllD_race():
     prior = 0.5
@@ -248,9 +264,9 @@ def Pavlov_gTFT_race():
                          file_name = 'horse_rounds_with_random_tremble=%.2f' % t)
 
 if __name__ == "__main__":
-    AllC_AllD_race()
-    Pavlov_gTFT_race()
-    assert 0
+    #AllC_AllD_race()
+    #Pavlov_gTFT_race()
+    #assert 0
 
     NRA = NiceReciprocalAgent
     MRA = ReciprocalAgent
@@ -260,6 +276,12 @@ if __name__ == "__main__":
     AD = AllD
     TFT = gTFT(y=1,p=1,q=0)
     GTFT = gTFT(y=1,p=.99,q=.33)
-    RA = MRA(RA_prior = .5, agent_types = (MRA, AC, AD, RandomAgent))
-    everyone = (RA, AC, AD)
-    limit_param_plot('bc',everyone)
+    RA = MRA(RA_prior = .5, RA_K = 2, agent_types = (MRA, AC, AD))
+    contest = (RA, AC, AD)
+    
+    everyone_ToM = ('self', AC, AD, TFT, gTFT, Pavlov, RandomAgent)
+    everyone_RA = MRA(RA_prior = .5, RA_K = 2, agent_types = everyone_ToM)
+    everyone = (everyone_RA, AC, AD, TFT, gTFT, Pavlov)
+    #limit_param_plot('bc',everyone)
+    bc_rounds_plot(everyone, trials = 20)
+    #limit_param_plot('rounds', everyone)
