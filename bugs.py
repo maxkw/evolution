7e60855462da9e3974c2ee09ee0b82bc6c239d52
Alejandro Vientos <polyguo@gmail.com>
2018-02-12 16:01:02 -0500

    added 'analyses' which runs the 'direct' game as every combination of simulated/analytic and limit/complete, all plots in the folder created should look the same

--- bugs.py
+++ bugs.py
@@ -1,5 +1,6 @@
 from agents import SelfishAgent,AltruisticAgent,WeAgent
 from evolve import limit_param_plot
+from itertools import product
 
 bugs_dir = "./plots/bugs/"
 
@@ -31,15 +32,48 @@ def variable_plots():
         #parallelized = False,
     )
     
-    file_name = 'bugged if different every time'
+    file_name = 'variable_plots - bugged if different every time'
     limit_param_plot("rounds",
                      file_name = file_name,
                      plot_dir = bugs_dir,
                      ##stacked = True,
                      **background_params)
 
+def analyses():
+    opponents = (
+        SelfishAgent,
+    )
+    ToM = ('self', ) + opponents
+    
+    
+    W = WeAgent(agent_types = ToM, prior = .5,)
+    pop = (W,)+opponents
+
+
+    background_params = dict(
+        trials = 20,
+        game = 'direct', rounds = 10,
+        tremble = 0,
+        
+
+        player_types = pop,
+        beta = 5,
+        pop_size = 10,
+        s = .5, 
+    )
+
+    for analysis,direct in product(['limit','complete'],[True,False]):
+        file_name = "ssd_v_rounds(analysis_type = %s, analytic = %s)" % (analysis,direct)
+        limit_param_plot("rounds",
+                         file_name = file_name,
+                         analysis_type = analysis,
+                         direct = direct,
+                         plot_dir = bugs_dir+"analyses - should all look the same/",
+                         **background_params)
+
 def main():
     variable_plots()
+    analyses()
 
 if __name__ == "__main__":
     main()
