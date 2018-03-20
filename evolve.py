64a689f20cf0d22eb5f683fc94cc8427b3198567
Max Kleiman-Weiner <maxhkw@gmail.com>
2018-03-20 13:48:04 -0400

    cleaned up evolution for the agents sims a bit

--- evolve.py
+++ evolve.py
@@ -16,100 +16,8 @@ from agents import WeAgent
 from steady_state import evo_analysis, simulation
 from steady_state import simulation_from_dict, matchups_and_populations
 from multiprocessing import Pool
-#from steady_state import cp_to_transition, complete_softmax, matchups_and_populations, sim_to_rmcp
 
 TODAY = "./plots/"+date.today().isoformat()+"/"
-
-def agent_sim(payoff, pop, s, mu):
-    pop_size = sum(pop)
-    type_count = len(payoff)
-    I = np.identity(type_count)
-    pop = np.array(pop)
-    assert type_count == len(pop)
-    while True:
-        yield pop
-        fitnesses = [np.dot(pop - I[t], payoff[t]) for t in xrange(type_count)]
-        fitnesses = softmax(fitnesses, s)
-        # fitnesses = [np.exp(s*np.dot(pop - I[t], payoff[t])) for t in xrange(type_count)]
-        # total_fitness = sum(fitnesses)
-        actions = [(b,d) for b,d in permutations(xrange(type_count),2) if pop[d]!=1]
-        probs = []
-        for b,d in actions:
-            death_odds = pop[d] / pop_size
-            # birth_odds = (1-mu) * fitnesses[b] / total_fitness + mu * (1/type_count)
-            birth_odds = (1-mu) * fitnesses[b] + mu * (1/type_count)
-            prob = death_odds * birth_odds
-            probs.append(prob)
-        actions.append((0, 0))
-        probs.append(1 - np.sum(probs))
-
-        probs = np.array(probs)
-        action_index = np.random.choice(len(probs), 1, p = probs)[0]
-        (b,d) = actions[action_index]
-        pop = pop + I[b]-I[d]
-
-
-
-@experiment(unpack = 'record', memoize = False)
-def agent_simulation(generations, pop, player_types, **kwargs):
-    payoffs = matchup_matrix(player_types = player_types, **excluding_keys(kwargs, 's', 'mu'))
-    params = default_params(**kwargs)
-    populations = agent_sim(payoffs, pop, params['s'], params['mu'])
-
-    record = []
-
-    # Populations is an infinite iterator so need to combine it with a
-    # finite iterator which sets the number of generations to look at.
-    for n, pop in izip(xrange(generations), populations):
-        for t, p in zip(player_types, pop):
-            record.append({'generation' : n,
-                           'type' : t.short_name('agent_types'),
-                           'population' : p})
-    return record
-
-
-
-# @plotter(agent_simulation,plot_exclusive_args = ['data'])
-# def sim_plotter(generations, pop, player_types, data =[]):
-#     for hue in data['type'].unique():
-#         plt.plot(data[data['type']==hue]['generation'],
-#                  data[data['type']==hue]['population'].astype(int),
-#                  label=hue)
-
-#     plt.legend()
-#     plt.xlabel(r'Time $\leftarrow$')
-#     plt.ylabel('Count')
-#     sns.despine()
-#     plt.tight_layout()
-
-def complete_agent_sim(player_types, s, **kwargs):
-    type_to_index = dict(map(reversed, enumerate(sorted(player_types))))
-    original_order = np.array([type_to_index[t] for t in player_types])
-    #player_types = sorted(player_types)
-    types, initial_pop = zip(*player_types)
-    print types
-    print sorted(types)
-    pop_size = sum(initial_pop)
-
-    rmcp = sim_to_rmcp(types, pop_size = pop_size, analysis_type = 'complete', **kwargs)
-    rcp = np.array([mcp[0] for mcp in rmcp])
-    _, populations = matchups_and_populations(player_types, pop_size, analysis_type = 'complete')
-
-    softmax_rcp = complete_softmax(rcp,populations,s)
-    transition = cp_to_transition(softmax_rcp[-1], populations, pop_size = pop_size, **kwargs).T
-
-    pop_ids = np.array(range(len(populations)))
-    pop_to_id = dict(map(reversed,enumerate(populations)))
-    id_to_pop = dict(enumerate(populations))
-
-    pop_id = pop_to_id[initial_pop]
-    while True:
-        yield np.array(id_to_pop[pop_id])
-        t = transition[pop_id]
-        neighbors = pop_ids[t!=0]
-        trans_probs = t[t!=0]
-        pop_id = np.random.choice(neighbors, p=trans_probs)
-
 
 def complete_sim_live(player_types, start_pop, s=1, mu = .000001, seed = 0, **kwargs):
     pop_size = sum(start_pop)
@@ -126,44 +34,36 @@ def complete_sim_live(player_types, start_pop, s=1, mu = .000001, seed = 0, **kw
 
     type_to_index = dict(map(reversed, enumerate(sorted(player_types))))
     original_order = np.array([type_to_index[t] for t in player_types])
-    #player_types = sorted(player_types)
+
     @memoize
     def sim(pop):
-        #print np.array(range(3))[original_order]
         f = simulation(zip(player_types,pop), **kwargs)[-1]
-        print 'types', player_types
 
-        print 'sorted', sorted(player_types)
-        print 'fixed', np.array(sorted(player_types))[original_order]
-        print "pop", pop
         non_players = np.array(pop)==0
-        print "raw", f
+
         player_payoffs = f[non_players==False]
         f[non_players == False] = softmax(player_payoffs, s)
         f[non_players] = 0
-        print "soft", f
-        #assert 0
         
         return f
 
     np.random.seed(seed)
     while True:
         yield pop
+
         fitnesses = sim(pop)
         actions = [(b,d) for b,d in permutations(xrange(type_count),2) if pop[d]!=0]
         probs = []
         for b,d in actions:
             death_odds = pop[d] / pop_size
-            # birth_odds = (1-mu) * fitnesses[b] / total_fitness + mu * (1/type_count)
             birth_odds = (1-mu) * fitnesses[b] + mu * (1/type_count)
             prob = death_odds * birth_odds
             probs.append(prob)
+            
         actions.append((0, 0))
         probs.append(1 - np.sum(probs))
 
         probs = np.array(probs)
-        #print actions
-        #print probs
         action_index = np.random.choice(len(probs), 1, p = probs)[0]
         (b,d) = actions[action_index]
         pop = map(int,pop + I[b]-I[d])
@@ -188,7 +88,7 @@ def complete_sim_plot(generations, player_types, data =[], graph_kwargs={}, **kw
     data['population'] = data['population'].astype(int)
     data = data[['generation', 'population', 'type']].pivot(columns='type', index='generation', values='population')
     type_order = dict(map(reversed,enumerate([t.short_name('agent_types') for t in player_types])))
-    data.reindex_axis(sorted(data.columns, key = lambda t:type_order[t]), 1)
+    data.reindex(sorted(data.columns, key = lambda t:type_order[t]), axis = 1)
 
     fig, ax = plt.subplots(figsize = (3.5,3))
     data.plot(ax = ax, legend=False,
@@ -360,9 +260,9 @@ def make_legend():
 
     return legend
 
-@plotter(ssd_v_param, plot_exclusive_args = ['experiment','data', 'stacked', 'graph_kwargs'])
-def limit_param_plot(param, player_types, data = [], stacked = False, graph_kwargs={}, **kwargs):
-    fig, ax = plt.subplots()
+@plotter(ssd_v_param, plot_exclusive_args = ['experiment','data', 'stacked', 'graph_kwargs', 'graph_funcs'])
+def limit_param_plot(param, player_types, data = [], stacked = False, graph_funcs=None, graph_kwargs={}, **kwargs):
+    fig, ax = plt.subplots(figsize = (3.5, 3))
 
     # TODO: Investigate this, some weird but necessary data cleaning
     data[data['proportion']<0] = 0
@@ -370,15 +270,10 @@ def limit_param_plot(param, player_types, data = [], stacked = False, graph_kwar
 
     data = data[[param, 'proportion', 'type']].pivot(columns='type', index=param, values='proportion')
     type_order = dict(map(reversed,enumerate([t.short_name('agent_types') for t in player_types])))
-    data.reindex_axis(sorted(data.columns, key = lambda t:type_order[t]), 1)
+    data.reindex(sorted(data.columns, key = lambda t:type_order[t]), axis = 1)
 
     if stacked:
-        if param in ['expected_interactions']:
-            data.plot.area(stacked = True, ylim = [0, 1], figsize = (3.5, 3), legend=False, **graph_kwargs)
-        elif param in ['rounds']:
-             data.plot.area(stacked = True, ylim = [0, 1], figsize = (3.5, 3), legend=False, **graph_kwargs)
-        else:
-            data.plot.area(stacked = True, ylim = [0, 1], figsize = (3.5, 3), legend=False, **graph_kwargs)
+        data.plot.area(stacked = True, ax=ax, ylim = [0, 1], legend=False, **graph_kwargs)
             
         if 'legend' not in graph_kwargs or graph_kwargs['legend']:
             legend = make_legend()
@@ -402,7 +297,7 @@ def limit_param_plot(param, player_types, data = [], stacked = False, graph_kwar
 
         elif param == 'tremble':
             plt.xlabel(r'Noise Probability ($\epsilon$)')
-            
+
         else:
             plt.xlabel(param)
 
@@ -425,7 +320,10 @@ def limit_param_plot(param, player_types, data = [], stacked = False, graph_kwar
         
     plt.yticks([0, 0.5, 1])
     plt.ylabel('Frequency')
-        
+
+    if graph_funcs is not None:
+        graph_funcs(ax)
+    
     sns.despine()
     plt.tight_layout()
 
