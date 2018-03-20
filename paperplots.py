8335dc7801b4ed7bb5679561c34126e0e023afc5
Max Kleiman-Weiner <maxhkw@gmail.com>
2018-03-20 16:23:21 -0400

    updateed the scenario plots to use the new colors and to look publication ready

--- paperplots.py
+++ paperplots.py
@@ -4,11 +4,15 @@ import seaborn as sns
 
 import agents as ag
 from evolve import limit_param_plot, complete_sim_plot
+from scenarios import scene_plot, reciprocal_scenarios_0, reciprocal_scenarios_1
+from experiment_utils import MultiArg
 
 PLOT_DIR = "./plots/"+inspect.stack()[0][1][:-3]+"/"
 TRIALS = 10
+BETA = 5
+PRIOR = 0.5
 
-def color_list(agent_list):
+def color_list(agent_list, sort = True):
     '''takes a list of agent types `agent_list` and returns the correctly
     ordered color mapping for plots
     '''
@@ -21,8 +25,11 @@ def color_list(agent_list):
         if 'GTFT' in a: return 'C4'
         if 'TFT' in a: return 'C5'
         raise('Color not defined for agent %s' % a)
-
-    return sns.color_palette([lookup(a) for a in sorted(agent_list, key=str)])
+    
+    if sort:
+        return sns.color_palette([lookup(a) for a in sorted(agent_list, key=str)])
+    else: 
+        return sns.color_palette([lookup(a) for a in agent_list])
 
 '''
 Evolution of Cooperation in the Game Engine:
@@ -31,17 +38,13 @@ Evolution of Cooperation in the Game Engine:
 3. TODO what about b/c, what about # of actions?
 '''
 def game_engine():
-    beta = 5
-    prior = 0.5
-    opponents = (ag.SelfishAgent(beta = beta), ag.AltruisticAgent(beta = beta))
+    opponents = (ag.SelfishAgent(beta = BETA), ag.AltruisticAgent(beta = BETA))
     ToM = ('self',) + opponents
-    agents = (ag.WeAgent(prior = prior, beta = beta, agent_types = ToM),) + opponents
+    agents = (ag.WeAgent(prior = prior, beta = BETA, agent_types = ToM),) + opponents
 
     common_params = dict(
         game = "game_engine",
         player_types = agents,
-        # observability = 0,
-        #analysis_type = 'complete',
         s = .5,
         pop_size = 10,
         trials = TRIALS,
@@ -50,47 +53,64 @@ def game_engine():
         graph_kwargs = {'color' : color_list(agents)},
     )
 
-    from evolve import params_heat
-    ticks = 4
-    params = {'expected_interactions': np.round(np.linspace(1, 4, ticks)),
-              'observability': np.round(np.linspace(0, 1, ticks), 2)}
+    ticks = 50
     
-    # Heatmap based on gamma vs. observability
-    params_heat(params,
-                tremble = 0,
-                file_name = 'game_engine_indirect_direct',
-                **common_params)
+    # from evolve import params_heat
+    # params = {'expected_interactions': np.round(np.linspace(1, 4, ticks)),
+    #           'observability': np.round(np.linspace(0, 1, ticks), 2)}
     
-    # # Vary expected number of repetitions
-    # limit_param_plot(
-    #     param = 'expected_interactions',
-    #     param_vals = np.round(np.linspace(1, 10, 10), 2),
-    #     tremble = 0.0, 
-    #     file_name = 'game_engine_gamma',
-    #     **common_params)
+    # # Heatmap based on gamma vs. observability
+    # params_heat(params,
+    #             tremble = 0,
+    #             file_name = 'game_engine_indirect_direct',
+    #             **common_params)
+    
+    # Expected number of interactions
+    limit_param_plot(
+        param = 'expected_interactions',
+        param_vals = np.round(np.linspace(1, 10, ticks), 2),
+        tremble = 0.0, 
+        analysis_type = 'limit',
+        file_name = 'game_engine_gamma',
+        **common_params)
 
     # # Vary tremble
     # limit_param_plot(
     #     param = 'tremble',
-    #     param_vals = np.round(np.linspace(0, 1, 10), 2),
-    #     expected_interactions = 5,
+    #     param_vals = np.round(np.linspace(0, 1, ticks), 2),
+    #     expected_interactions = 10,
+    #     analysis_type = 'limit',
     #     file_name = 'game_engine_tremble',
     #     **common_params)
+
+    # # Agent Sim Plots
+
+    # sim_params = dict(
+    #     generations = 60000,
+    #     mu = 0.001,
+    #     start_pop = (0, 10, 0),
+    #     tremble = 0,
+    #     **common_params)
     
-    complete_sim_plot(
-         generations = 15000,
-         #param = 'rounds',
-         mu = .001,
-         tremble = 0,
-         expected_interactions = 5,
-         start_pop = (0,10,0),
-         file_name = 'game_engine_sim',
-         # seed = 0,
-         **common_params)
+    # complete_sim_plot(
+    #     expected_interactions = 10,
+    #     observability = 0,
+    #     file_name = 'game_engine_sim_direct',
+    #     seed = 0,
+    #     **sim_params)
+
+    # complete_sim_plot(
+    #     expected_interactions = 1,
+    #     observability = 1,
+    #     file_name = 'game_engine_sim_indirect',
+    #     seed = 0,
+    #     **sim_params)
+
+    
 def ipd():
     old_pop = (ag.AllC,ag.AllD,ag.GTFT,ag.TFT,ag.WSLS)
     ToM = ('self',) + old_pop
-    new_pop = old_pop + (ag.WeAgent(prior = .5, beta = 5, agent_types = ToM),)
+    new_pop = old_pop + (ag.WeAgent(prior = PRIOR, beta = BETA, agent_types = ToM),)
 
     common_params = dict(
         game = 'direct',
@@ -125,22 +145,57 @@ def ipd():
             graph_kwargs = {'color' : color_list(player_types)},
             **common_params)
 
+    # Cognitive Costs
+    def cog_cost_graph(ax):
+        vals = ax.get_xticks()
+        surplus = common_params['benefit'] - common_params['cost']
+        ax.set_xticklabels(['{:3.0f}%'.format(x / surplus * 100) for x in vals])
+        ax.set_xlabel('Cognitive Cost \n% of (b-c)')
     
-    # Adding cognitive costs
     limit_param_plot(param = 'cog_cost',
-                     tremble = 0.0,
-                     rounds = 10,
+                     tremble = 0,
+                     rounds = 50,
                      param_vals = np.linspace(0, .3, 50),
                      file_name = "ipd_cogcosts",
                      player_types = new_pop,
+                     graph_funcs = cog_cost_graph,
                      graph_kwargs = {'color' : color_list(new_pop)},
                      **common_params)
 
+def agent():
+    opponents = (ag.AltruisticAgent(beta = BETA), ag.SelfishAgent(beta = BETA))
+    ToM = ('self',) + opponents
+    agents = (ag.WeAgent(prior = PRIOR, beta = BETA, agent_types = ToM),) + opponents
+
+    common_params = dict(
+        agent_types = ToM,
+        plot_dir = PLOT_DIR,
+        color = color_list(agents, sort=False)
+    )
+
+    # SCENARIO PLOTS
+    scene_plot(
+        scenario_func=reciprocal_scenarios_0,
+        titles = ['Prior' + "\n ",
+                  r'$j$ cooperates w/ $k$' + "\n ",
+                  r'$j$ defects on $k$' + "\n "],
+        file_name='scene_reciprocal_0',
+        **common_params)
+
+    scene_plot(
+        scenario_func=reciprocal_scenarios_1,
+        titles = [r'$k$ defects on $j$' + "\n" + r'$j$ defects on $k$',
+                  r'$k$ defects on $j$' + "\n" + r'$j$ cooperates w/ $k$', 
+                  r'$k$ cooperates w/ $j$' + "\n" + r'$j$ defects on $k$'],
+        file_name='scene_reciprocal_1',
+        **common_params)
+
     
-    
+
 if __name__ == '__main__':
-    game_engine()
+    # game_engine()
     # ipd()
+    agent()
 
 
 
@@ -152,34 +207,5 @@ if __name__ == '__main__':
 
 
 # plot_coop_prob(file_name = 'coop_prob', **params)
-# scenarios.main(**params)
 # indirect.ToM_indirect(**params)
     
-
-# # Currently broken
-# evolve.AllC_AllD_race()
-# evolve.Pavlov_gTFT_race()
-
-
-
-
-# condition = dict(trials=50,
-#                  plot_dir=plot_dir,
-#                  beta=5,
-# )
-
-
-
-# belief_plot(believed_type=ReciprocalAgent,
-#             player_types=ReciprocalAgent, priors=.75, Ks=1, **condition)
-# belief_plot(experiment=forgiveness, believed_type=ReciprocalAgent,
-#             player_types=ReciprocalAgent, priors=.75, Ks=1, **condition)
-# joint_fitness_plot(**condition)
-# reward_table(player_types=ReciprocalAgent, size=11, Ks=0, **condition)
-# reward_table(player_types=ReciprocalAgent, size=11, Ks=1, **condition)
-
-
-# scene_plot(RA_prior=.75, RA_K=MultiArg([0, 1]))
-
-# pop_fitness_plot((ReciprocalAgent, SelfishAgent), proportion=MultiArg([float(i) / 10 for i in range(10)[1:]]), Ks=MultiArg(
-#     range(3)), plot_dir=plot_dir, trials=500, agent_types=(ReciprocalAgent, SelfishAgent), min_pop_size=50, beta=1)
